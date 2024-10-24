fa694bfffeb5aeda500d9bf28f94e1864b9b559b
isaiasvas <isaias@visioit.com.br>
2024-10-24 02:51:42 -0300

    :sparkles: feat: Hero e Projetos

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     // Carrega o header, content e footer
-    $('#header').load('layouts/sections/header.html');
-    $('#hero').load('layouts/sections/hero.html');
-    $('#services').load('layouts/sections/services.html');
-    $('#skills').load('layouts/sections/skills.html');
-    $('#footer').load('layouts/sections/footer.html');
+    $('#header').load('./layouts/header.html');
+    $('#hero').load('./layouts/sections/hero.html');
+    $('#services').load('./layouts/sections/services.html');
+    $('#skills').load('./layouts/sections/skills.html');
+    $('#footer').load('./layouts/footer.html');
 });
