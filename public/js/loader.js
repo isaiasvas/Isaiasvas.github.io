9ee62a683756943a4784435ae30ee738101e9e3d
Isaias Silva <78052574+isaiasvas@users.noreply.github.com>
2026-07-14 19:09:58 -0300

    Delete public directory

--- public/js/loader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Carrega o header, content e footer
-    $('#header').load('./layouts/header.html');
-    $('#hero').load('./layouts/sections/hero.html');
-    $('#services').load('./layouts/sections/services.html');
-    $('#skills').load('./layouts/sections/skills.html');
-    $('#footer').load('./layouts/footer.html');
-});
